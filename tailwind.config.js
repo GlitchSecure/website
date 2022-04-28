9ed39c7f726f30218dcf84414418e94484846a11
GlitchWitch <hello@glitchwitch.io>
2022-04-28 09:27:39 +0000

    Fix tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
   theme: {
-    colors: {
-      'dark-purple': '#1d162b',
-      'medium-purple': '#271d3a',
-    },
     extend: {
+      colors: {
+        'dark-purple': '#1d162b',
+        'medium-purple': '#271d3a',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       }
