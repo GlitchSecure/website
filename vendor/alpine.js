bf64b74f24674262fc7b4c677a0953542ac0272e
GlitchWitch <jade@glitchsecure.com>
2023-06-25 13:53:39 -0500

    Fix alpine.js building

--- vendor/alpine.js
+++ vendor/alpine.js
@@ -1,15 +1,15 @@
 import Alpine from "alpinejs";
-import tooltip from './alpine/tooltip'
-import pricing from './alpine/pricing'
+//import tooltip from './alpine/tooltip'
+//import pricing from './alpine/pricing'
 import getStartedForm from './alpine/get-started-form'
 
 window.Alpine = Alpine
 
 // Register directives
-Alpine.directive('tooltip', tooltip)
+//Alpine.directive('tooltip', tooltip)
 
 // Register datasets
-Alpine.data('pricing', pricing)
+//Alpine.data('pricing', pricing)
 Alpine.data('getStartedForm', getStartedForm)
 
 Alpine.start()
