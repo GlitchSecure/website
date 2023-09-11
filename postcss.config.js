af1f00be94cfdb1ddd83f9b03c7f288057cb9d62
GlitchWitch <jade@glitchsecure.com>
2023-09-11 10:42:55 -0500

    Add blog post dark mode fixes and css comments

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss")("./tailwind.config.js"),
     require("postcss-import"),
+    require("tailwindcss/nesting"),
     require("autoprefixer"),
   ],
 };
